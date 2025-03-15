7d94a5ff5a89af7810015c2ee51855e67653b138
Chris <cshkokota@gmail.com>
2025-03-15 14:29:27 -0400

    First

---
+++ src/main/java/net/kawws/kawwsmod/KawwsMod.java
@@ -0,0 +1,16 @@
+package net.kawws.kawwsmod;
+
+import net.fabricmc.api.ModInitializer;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public class KawwsMod implements ModInitializer {
+	public static final String MOD_ID = "kawwsmod";
+	public static final Logger LOGGER = LoggerFactory.getLogger(MOD_ID);
+
+	@Override
+	public void onInitialize() {
+
+	}
+}
---
+++ src/main/java/net/kawws/kawwsmod/KawwsModClient.java
@@ -0,0 +1,10 @@
+package net.kawws.kawwsmod;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class KawwsModClient  implements ClientModInitializer {
+    @Override
+    public void onInitializeClient() {
+
+    }
+}
